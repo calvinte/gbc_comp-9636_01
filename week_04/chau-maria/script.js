49156b446f9cdfe41cd9bcbbe245fe2bc1b49e6f
chaugirl <chau.marialuisa@gmail.com>
2014-10-16 12:46:15 -0400

    16Oct2014
    Assignment

--- week_04/chau-maria/script.js
+++ week_04/chau-maria/script.js
@@ -1,43 +1,46 @@
-angular.module('guestApp', []).controller('GuestsController', ['$scope', function($scope) {
+angular.module('menuOrderApp', []).controller('menuOrdersController', ['$scope', function($scope) {
 	//localStorage.setItem('students', 'testing');
-	console.log(localStorage.getItem('guests'));
+	console.log(localStorage.getItem('menuorders'));
 	//return;
 
-	$scope.guests = JSON.parse(localStorage.getItem('guests'))  || [{
-		last_name: 'Chau', 
-		first_name: 'Maria',
-		twitter_username: 'malouchau',
+	$scope.menuorders = JSON.parse(localStorage.getItem('menuorders'))  || [{
+		guest_name: 'Chau', 
+		appetizer: 'Scallops',
+		main_course: 'Salmon',
 		number: 1,
-		dish_requested: 'Salmon'
+		drink: 'Martini',
+		dessert: 'Cheesecake', 
 	}];
 	
-	$scope.addGuest = function() {
-		console.log($scope.first_name);
-		$scope.guests.push({
-			number: $scope.guests.length + 1,
-			first_name: $scope.first_name, 
-			last_name: $scope.last_name, 
-			twitter_username: $scope.twitter_username,
-			dish_requested: $scope.dish_requested,
+	$scope.addMenuOrder = function() {
+		console.log($scope.guest_name);
+		$scope.menuorders.push({
+			number: $scope.menuorders.length + 1,
+			guest_name: $scope.guest_name, 
+			appetizer: $scope.appetizer, 
+			main_course: $scope.main_course,
+			drink: $scope.drink,
+			dessert: $scope.dessert,
 		});
 
-		$scope.first_name = '';
-		$scope.last_name = '';
-		$scope.twitter_username = '';
-		$scope.dish_requested = '';
+		$scope.guest_name = '';
+		$scope.appetizer = '';
+		$scope.main_course = '';
+		$scope.drink = '';
+		$scope.dessert = '';
 
-		localStorage.setItem('guests', JSON.stringify($scope.guests));
+		localStorage.setItem('menuorders', JSON.stringify($scope.menuorders));
 	}
-	$scope.removeGuest = function(guest) {
-		var arrayIndex = $scope.guests.indexOf(guest);
-		$scope.guests.splice(arrayIndex, 1);
+	$scope.removeMenuOrder = function(menuorder) {
+		var arrayIndex = $scope.menuorders.indexOf(menuorder);
+		$scope.menuorders.splice(arrayIndex, 1);
 		
-		$scope.guests.forEach(function(guest, i) {
-			guest.number = i + 1;
-			console.log(guest, i);
+		$scope.menuorders.forEach(function(menuorder, i) {
+			menuorder.number = i + 1;
+			console.log(menuorder, i);
 		});
 
-		localStorage.setItem('guests', JSON.stringify($scope.guests));
+		localStorage.setItem('menuorders', JSON.stringify($scope.menuorders));
 
 	};
 }]); 
